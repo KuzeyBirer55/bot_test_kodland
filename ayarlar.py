c5241dd063beaf3f33f9c7431d8981465cd0f14a
KuzeyBirer55 <147754038+KuzeyBirer55@users.noreply.github.com>
2024-08-30 17:35:52 +0300

    İlk dosyalar yüklendi
    Yazı-Tura atabilen, gülen, selam veren veşifre oluşturabilen bir discord bot eklendi.
    Komutlar:
    $gül
    $selam
    $para
    $şifre

---
+++ ayarlar.py
@@ -0,0 +1,6 @@
+#settings.py in original
+
+ayarlar = {
+    "on_taki": "$", #ön takı, prefix (maybe it won't work if we call it different here)
+    "TOKEN": "MTI3NDA2OTg2MTc3NzAxODg5MA.G_nKqW.DboRsevuULzVfDHqeEMkKtsnnSWlzPxsNB-_RY"
+}
